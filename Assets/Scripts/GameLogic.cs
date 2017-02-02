18df6774a8e5f5aa4f65d9eae4be8f5a7cff8294
Reuben K. Coutinho <reuben.coutinho@gmail.com>
2017-02-02 11:26:29 +1000

    Chore: added Scripts folder and added gamelogic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour {
+
+	public GameObject firstUI, secondUI;
+
+	// Use this for initialization
+	void Start () {
+		secondUI.SetActive (false);
+		if(!firstUI.activeSelf)firstUI.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showSecondPanel(){
+		firstUI.SetActive (false);
+		secondUI.SetActive (true);
+	}
+
+	public void showNextScene(){
+		SceneManager.LoadSceneAsync ("Office", LoadSceneMode.Single);
+	}
+
+}
