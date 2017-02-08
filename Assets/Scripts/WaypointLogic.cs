b43633410655ca63a61de3334271e499ccf85d4f
Reuben K. Coutinho <reuben.coutinho@gmail.com>
2017-02-08 11:36:10 +1000

    Refactor: Removed unused code

--- Assets/Scripts/WaypointLogic.cs
+++ Assets/Scripts/WaypointLogic.cs
@@ -5,50 +5,32 @@ using UnityEngine;
 public class WaypointLogic : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject firstWP;
-	public GameObject secondWP;
-	public GameObject thirdWP;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player.transform.position == firstWP.transform.position) {
-			firstWP.SetActive (false);
-		}
-		if (player.transform.position == secondWP.transform.position) {
-			secondWP.SetActive (false);
-		}
-		if (player.transform.position == thirdWP.transform.position) {
-			thirdWP.SetActive (false);
-		}
 	}
 
 	public void MoveToWaypoint(GameObject waypoint){
-		Hashtable hashtable = new Hashtable();
-		hashtable.Add("time", 3f);
-		hashtable.Add("position", waypoint.transform.position);
-		hashtable.Add("onComplete", "ShowWaypoints");
-		hashtable.Add("onCompleteTarget",this.gameObject);
-		hashtable.Add("onStart", "HideWaypoints");
-		hashtable.Add("onStartTarget",this.gameObject);
-		iTween.MoveTo( player, hashtable );
+		if (player.transform.position != waypoint.transform.position) {
+			Hashtable hashtable = new Hashtable ();
+			hashtable.Add ("time", 3f);
+			hashtable.Add ("position", waypoint.transform.position);
+//			hashtable.Add ("onComplete", "ShowWaypoints");
+//			hashtable.Add ("onCompleteTarget", this.gameObject);
+//			hashtable.Add ("onStart", "HideWaypoints");
+//			hashtable.Add ("onStartTarget", this.gameObject);
+			iTween.MoveTo (player, hashtable);
+		}
 	}
 
-	void HideWaypoints(){
-		thirdWP.SetActive (false);
-		secondWP.SetActive (false);
-		firstWP.SetActive (false);
-	}
-
-	void ShowWaypoints(){
-		print ("ShowWaypoints");
-		thirdWP.SetActive (true);
-		secondWP.SetActive (true);
-		firstWP.SetActive (true);
-	}
+//	void HideWaypoints(){
+//	}
+//
+//	void ShowWaypoints(){
+//	}
 
 }
