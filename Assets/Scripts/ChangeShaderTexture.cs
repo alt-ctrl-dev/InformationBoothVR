1d8e04c169a7a6abff66aea0958f973dc1ecd484
Reuben K. Coutinho <reuben.coutinho@gmail.com>
2017-02-15 10:50:53 +1000

    Chore: Created script for changing texture of material dynamically

---
+++ Assets/Scripts/ChangeShaderTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShaderTexture : MonoBehaviour {
+
+	public Material shader;
+
+	public Texture[] textures;
+
+	private int index;
+
+	// Use this for initialization
+	void Start () {
+		index = (int)Random.Range (0f, textures.Length);
+		ChangeImage ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeImage(){
+		shader.SetTexture ("_Tex", textures [index]);
+		index++;
+		if (index >= textures.Length)
+			index = 0;
+	}
+}
